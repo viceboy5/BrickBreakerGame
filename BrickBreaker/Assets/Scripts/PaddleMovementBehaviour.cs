23c7ebdd4f6f6dd11b674ee6548d699bfcfb0c4c
viceboy5 <10811528@my.uvu.edu>
2023-09-22 13:33:32 -0600

    adding changes to draggable to paddlemovement instead

---
+++ BrickBreaker/Assets/Scripts/PaddleMovementBehaviour.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PaddleMovementBehaviour : MonoBehaviour
+{
+    private Camera cameraObj;
+    public bool draggable;
+    public Vector3 position, offset;
+    public UnityEvent startDragEvent, endDragEvent;
+    void Start()
+    {
+        cameraObj = Camera.main;
+    }
+
+    public IEnumerator OnMouseDown()
+    {
+        offset = transform.position - cameraObj.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraObj.transform.position.y));
+        draggable = true;
+        startDragEvent.Invoke();
+        yield return new WaitForFixedUpdate();
+
+        while (draggable)
+        {
+            yield return new WaitForFixedUpdate();
+            position = cameraObj.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraObj.transform.position.y)) + offset;
+            transform.position = position;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        draggable = false;
+        endDragEvent.Invoke();
+    }
+}
