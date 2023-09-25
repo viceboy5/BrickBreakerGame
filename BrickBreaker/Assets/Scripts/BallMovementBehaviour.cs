9495d73247313cc49d84f01b38cfc4aed6ef021b
viceboy5 <10811528@my.uvu.edu>
2023-09-25 14:35:01 -0600

    adjusted ball movement to hesitate and bounce correctly

--- BrickBreaker/Assets/Scripts/BallMovementBehaviour.cs
+++ BrickBreaker/Assets/Scripts/BallMovementBehaviour.cs
@@ -1,21 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Rigidbody))]
 public class BallMovementBehaviour : MonoBehaviour
 {
     private Rigidbody rigidbody;
     public float speed;
+    private Vector3 force = Vector3.zero;
     
     
-    void Awake()
+    private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
-    
-    public void SetVelocity()
+
+    private void Start()
     {
-        rigidbody.AddForce(Vector3.forward * Time.deltaTime * speed *100);
+        Invoke(nameof(SetVelocityRandomX), 1f);
+    }
+
+    public void SetVelocityRandomX()
+    {
+        force.x = Random.Range(-1f, 1f);
+        force.y = 0f;
+        force.z = -1f;
+        rigidbody.AddForce(force.normalized * Time.deltaTime * speed * 100);
     }
 }
